6c3c76b9e7ae913e2fe64e1ea05f74afbda27663
AndiS82 <19schneiderandreas82@gmail.com>
2022-11-22 11:46:17 +0100

    time converted

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,45 @@ fetch("https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.1180
         console.log(data.main.pressure)
 
         document.getElementById("ort").innerHTML = data.name;
-
+        //Die Angabe der API in Kelvin in ° Celsius umwandeln;
         kelvinToCelsius = data.main.temp - 273.15;
         kelvinToCelsiusRounded = kelvinToCelsius.toFixed(2);
         document.getElementById("temp").innerHTML = kelvinToCelsiusRounded + "° C";
+
+        //die Zeitangabe der API in MS in ein(e) für Normalsterbliche lesbares Datum/Zeitangabe umwandeln:
+        let localUnixTime = data.dt;
+        let currentTime;
+        console.log(localUnixTime);
+        function convertFromUnixTime() {
+            let localTime = new Date(localUnixTime);
+            currentTime = localTime.toLocaleTimeString("de-DE");
+            return currentTime;
+        };
+        convertFromUnixTime();
+        console.log(currentTime);
+
+        //sunrise
+        let sunriseTime = data.sys.sunrise;
+        let showSunrise;
+        console.log(sunriseTime);
+        function convertSunriseTime() {
+            let targetSunriseTime = new Date(sunriseTime * 1000);
+            showSunrise = targetSunriseTime.toLocaleTimeString("en-UK");
+            return showSunrise;
+        }
+        convertSunriseTime();
+        console.log(showSunrise);
+
+        //sunset
+        let sunsetTime = data.sys.sunset;
+        let showSunset;
+        console.log(sunsetTime);
+        function convertSunsetTime() {
+            let targetSunsetTime = new Date(sunsetTime * 1000);
+            showSunset = targetSunsetTime.toLocaleTimeString("en-UK");
+            return showSunset;
+        }
+        convertSunsetTime();
 
         let imageIcon = document.querySelector("#icon");
         console.log(imageIcon);
@@ -26,11 +61,11 @@ fetch("https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.1180
         //document.getElementById("condition").innerHTML = data.weather[0].description;
 
         let date = new Date();
-        document.getElementById("obtained").innerHTML = "Obtained at: " + date;
+        document.getElementById("obtained").innerHTML = currentTime;
 
         document.getElementById("timeValue").innerHTML = data.dt;
 
-        wind = "Speed: " + data.wind.speed + " Degree: " + data.wind.deg + " Gust: " + data.wind.gust
+        wind = "Speed: " + data.wind.speed + "<br></br>" + "Degree: " + data.wind.deg
         document.getElementById("windValue").innerHTML = wind;
 
         //cloudiness
@@ -47,12 +82,13 @@ fetch("https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.1180
 
         //sunrise
 
-        document.getElementById("sunriseValue").innerHTML = data.sys.sunrise;
+        document.getElementById("sunriseValue").innerHTML = showSunrise;
 
         //sunset
-        document.getElementById("sunsetValue").innerHTML = data.sys.sunset;
+        document.getElementById("sunsetValue").innerHTML = showSunset;
 
         //geoCoords
         let coords = "lat. " + data.coord.lat + " lon: " + data.coord.lon;
         document.getElementById("geoCoordsValue").innerHTML = coords;
     });
+
